4b34773da0b33e813295ca2a09778d204cc38420
Orasur Rahman <orasurrahman203@gmail.com>
2023-06-07 18:38:55 +0600

    UP

--- src/_mock/category.js
+++ src/_mock/category.js
@@ -17,11 +17,57 @@ import { useState, useEffect } from "react";
 import ModeEditIcon from '@mui/icons-material/ModeEdit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import TextField from '@mui/material/TextField';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Backdrop from "@mui/material/Backdrop";
 
+
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: "52%",
+  bgcolor: 'background.paper',
+  // border: '2px solid #000',
+  borderRadius:3,
+  boxShadow: 24,
+  p: 4,
+};
 
 
 const Category = () => {
   const [data, setData] = useState([]);
+  const [menu, setMenu] = useState(false);
+  // New category start from here 
+  const [opens, setOpens] = useState(false);
+  const handleOpen = () => setOpens(true);
+  const handleClickUpdate=()=>{
+    setOpens(false)
+  }
+  // const handleClickCancel=()=>{
+  //   setOpens(false)
+  // }
+  const handleClickedCategory=()=>{
+    handleOpen();
+  }
+  const handleClose = () => setOpens(false);
+
+// New category start from here 
+const [open, setOpen] = useState(false);
+const handleClickedSubcategory=()=>{
+  setOpen(true);
+}
+ 
+const handleClickCreate=()=>{
+  setOpen(false)
+}
+const handleSubcategory = () => setOpen(false);
 
 useEffect(() => {
   async function getData(){
@@ -43,6 +89,7 @@ const handleClickedDelete=()=>{
 }
 const handleClickedSubmenu=()=>{
   console.log("Menu button");
+  setMenu(!menu);
 }
 
 
@@ -52,57 +99,42 @@ const handleClickedSubmenu=()=>{
       <title> Category | admin dashboard </title>
     </Helmet>
     <Box sx={{marginX:5}}> 
-
-        <Typography variant='h2' sx={{padding:0}}>Category</Typography>
+    <Box sx={{ flexGrow: 1  }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+        <Typography variant='h3' sx={{padding:0}}>Category</Typography>
+        </Grid>
+        <Grid item xs={12} sm={6} sx={{justifyContent:"end"}}>
+        <Button onClick={handleClickedCategory} sx={{bgcolor:"#6610F2", color:"white",":hover": {
+                                bgcolor: '#6EAB49'
+                            }, marginX:2}}>NEW CATEGORY</Button>
+      <Button onClick={handleClickedSubcategory} sx={{bgcolor:"#6610F2", color:"white",":hover": {
+                                bgcolor: '#6EAB49'
+                            }}}>NEW SUBCATEGORY</Button>
+        </Grid>
+      
+      </Grid>
+    </Box>
         {
 data.map((item)=>(
   <>
-     {/* <Card sx={{ width: "64%" }}>
+     <Card sx={{ width: "72%",  marginTop:3}}>
      <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item xs={3}>
       <CardMedia
         component="img"
         alt="green iguana"
-        height="80"
+        height="100%"
         width="200"
-        image="./../../assets/electronicesProduct.jpeg"
-      />
-        </Grid>
-        <Grid item xs={3}>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-         {item.name}
-        </Typography>
-      </CardContent>
-      </Grid>
-      <Grid item xs={6}>
-      <CardActions>
-        <Button><ModeEditIcon/></Button>
-        <Button><DeleteIcon/></Button>
-        <Button><ArrowForwardIosIcon/></Button>
-      </CardActions>
-      </Grid>
-      </Grid>
-      </Box>
-    </Card> */}
-     <Card sx={{ width: "64%",  marginTop:3}}>
-     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={3}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="100"
-        width="200"
-        image="./../../assets/electronicesProduct.jpeg"
+        image="./../../assets/Rectangle 181.png"
       />
         </Grid>
         <Grid item xs={3}>
       <CardContent sx={{display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    height:'16vh',
+    height:'100%',
    }}>
         <Typography gutterBottom variant="h5" component="div">
          {item.name}
@@ -111,24 +143,88 @@ data.map((item)=>(
       </Grid>
       <Grid item xs={6}>
       <CardActions sx={{display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'end',
     alignItems: 'center',
-    height:'10vh',
+    height:'100%',
    }}>
-       <Button onClick={handleClickedEdit}><ModeEditIcon/></Button>
-      <Button onClick={handleClickedDelete}><DeleteIcon/></Button>
-      <Button onClick={handleClickedSubmenu}><ArrowForwardIosIcon/></Button>
+       <Button onClick={handleClickedEdit}><ModeEditIcon sx={{color:"#6EAB49"}}/></Button>
+      <Button onClick={handleClickedDelete}><DeleteIcon sx={{color:"#E53E3E"}}/></Button>
+      <Button onClick={handleClickedSubmenu}>{ menu ? <ArrowForwardIosIcon sx={{color:"#6610F2"}}/> : <KeyboardArrowDownIcon  sx={{color:"#6610F2"}}/>}</Button>
       </CardActions>
- {/* <ButtonGroup variant="contained" aria-label="outlined primary button group" sx={{display: 'flex',
+      </Grid>
+      </Grid>
+      </Box>
+    </Card>
+     <Card sx={{ width: "72%",  marginTop:3}}>
+     <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="100%"
+        width="200"
+        image="./../../assets/Rectangle 181.png"
+      />
+        </Grid>
+        <Grid item xs={3}>
+      <CardContent sx={{display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    height:'10vh',
+    height:'100%',
    }}>
-      <Button onClick={handleClickedEdit}><ModeEditIcon/></Button>
-      <Button onClick={handleClickedDelete}><DeleteIcon/></Button>
-      <Button onClick={handleClickedSubmenu}><ArrowForwardIosIcon/></Button>
-    </ButtonGroup> */}
-
+        <Typography gutterBottom variant="h5" component="div">
+         {item.name}
+        </Typography>
+      </CardContent>
+      </Grid>
+      <Grid item xs={6}>
+      <CardActions sx={{display: 'flex',
+    justifyContent: 'end',
+    alignItems: 'center',
+    height:'100%',
+   }}>
+       <Button onClick={handleClickedEdit}><ModeEditIcon sx={{color:"#6EAB49"}}/></Button>
+      <Button onClick={handleClickedDelete}><DeleteIcon sx={{color:"#E53E3E"}}/></Button>
+      <Button onClick={handleClickedSubmenu}>{ menu ? <ArrowForwardIosIcon sx={{color:"#6610F2"}}/> : <KeyboardArrowDownIcon  sx={{color:"#6610F2"}}/>}</Button>
+      </CardActions>
+      </Grid>
+      </Grid>
+      </Box>
+    </Card>
+     <Card sx={{ width: "72%",  marginTop:3}}>
+     <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="100%"
+        width="200"
+        image="./../../assets/Rectangle 181.png"
+      />
+        </Grid>
+        <Grid item xs={3}>
+      <CardContent sx={{display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height:'100%',
+   }}>
+        <Typography gutterBottom variant="h5" component="div">
+         {item.name}
+        </Typography>
+      </CardContent>
+      </Grid>
+      <Grid item xs={6}>
+      <CardActions sx={{display: 'flex',
+    justifyContent: 'end',
+    alignItems: 'center',
+    height:'100%',
+   }}>
+       <Button onClick={handleClickedEdit}><ModeEditIcon sx={{color:"#6EAB49"}}/></Button>
+      <Button onClick={handleClickedDelete}><DeleteIcon sx={{color:"#E53E3E"}}/></Button>
+      <Button onClick={handleClickedSubmenu}>{ menu ? <ArrowForwardIosIcon sx={{color:"#6610F2"}}/> : <KeyboardArrowDownIcon  sx={{color:"#6610F2"}}/>}</Button>
+      </CardActions>
       </Grid>
       </Grid>
       </Box>
@@ -137,6 +233,130 @@ data.map((item)=>(
 ))
 }
   </Box>
+
+  {/* New Category start from here */}
+  <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={opens}
+        onClose={handleClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={opens}>
+          <Box sx={style}>
+            {/* <Box
+                        display={'flex'}
+                        flexDirection={'column'}
+                        alignItems="center"
+                        justifyContent={'center'}
+                        margin='auto'
+                        maxWidth={540}
+                        // marginTop={5}
+                        //    padding={3}
+                        borderRadius={2}
+                        boxShadow={'5px 5px 10px #ccc'}
+                        sx={{
+                            ":hover": {
+                                boxShadow: '10px 10px 20px #ccc'
+                            }
+                        }}
+                    > */}
+                        {/* <Typography variant='h3'> New CATEGORY </Typography> */}
+                        {/* <Avatar sx={{width:100,height:100, marginTop:3}} alt="Travis Howard" src="../../assets/images/avatars/tree-736885_1280.jpg" /> */}
+                        {/* <Button variant="contained" sx={{bgcolor:"#A084DD",":hover": {
+                                bgcolor: '#A749FF'
+                            } , marginY:2, width:"72%"}}>UPDATE YOUR IMAGE</Button> */}
+                        <Box
+                        //    alignItems="center"
+                        //    justifyContent={'center'}
+                          >
+                   <Typography>
+                    Category 
+                   </Typography>
+                   <TextField id="outlined-basic" label="Category" variant="outlined" type="text" sx={{width:"100%", marginY:2}}/>
+                   <Button variant="contained" sx={{bgcolor:"#6610F2",":hover": {
+                                bgcolor: '#6EAB49'
+                            } , width:"32%",
+                            display:'flex',
+                            flexDirection:'column',
+                            alignItems:"center",
+                            justifyContent:'center',
+                            margin:'auto'
+                            }} onClick={handleClickUpdate}>CREATE</Button>
+                        </Box>
+                    </Box>
+          {/* </Box> */}
+        </Fade>
+  </Modal>
+
+{/* New SubCategory start from here */}
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleSubcategory}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            {/* <Box
+                        display={'flex'}
+                        flexDirection={'column'}
+                        alignItems="center"
+                        justifyContent={'center'}
+                        margin='auto'
+                        maxWidth={540}
+                        // marginTop={5}
+                        //    padding={3}
+                        borderRadius={2}
+                        boxShadow={'5px 5px 10px #ccc'}
+                        sx={{
+                            ":hover": {
+                                boxShadow: '10px 10px 20px #ccc'
+                            }
+                        }}
+                    > */}
+                        <Box
+                        //    alignItems="center"
+                        //    justifyContent={'center'}
+                          >
+                   <Typography>
+                    Category 
+                   </Typography>
+                   <TextField id="outlined-basic" label="Category" variant="outlined" type="text" sx={{width:"100%", marginY:2}}/>
+                   <Typography>
+                    SubCategory 
+                   </Typography>
+                   <TextField id="outlined-basic" label="SubCategory" variant="outlined" type="text" sx={{width:"100%", marginY:2}}/>
+                   <Button variant="contained" sx={{bgcolor:"#6610F2",":hover": {
+                                bgcolor: '#6EAB49'
+                            } , width:"32%",
+                            display:'flex',
+                            flexDirection:'column',
+                            alignItems:"center",
+                            justifyContent:'center',
+                            margin:'auto',
+              
+                            }} onClick={handleClickCreate}>CREATE</Button>
+                        </Box>
+                    </Box>
+          {/* </Box> */}
+        </Fade>
+      </Modal>
+
+
     </>
     );
 };
