efc7a6bc56bc92d0200f26e9fff08d41b03c4bc1
Orasur Rahman <orasurrahman203@gmail.com>
2023-07-09 13:28:04 +0600

    UP

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,3 +1,4 @@
+
 import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -23,20 +24,24 @@ import { getRequestHandler } from '../apiHandler/customApiHandler';
 
 export default function OrderList() {
 
-  const [orderList, setOrderList] = React.useState([])
-  const [selectedDeliveryStatus, setSelectedDeliveryStatus] = React.useState('user');
+  const [userList, setUserList] = React.useState([])
+  const [id,setId]= React.useState()
+  const [roleStatus, setRoleStatus] = React.useState('user');
   const [currentPage, setCurrentPage] = React.useState(1);
   const [totalPages, setTotalPages] = React.useState(0);
   const [show, setShow] = React.useState(false)
 
   async function handleGetAllDataforUpdate() {
     setShow(true);
-    // try {const response = await getRequestHandler(`https://marpapi.techanalyticaltd.com/admin/useraction?role=user`);
-    try {const response = await getRequestHandler(`https://marpapi.techanalyticaltd.com/admin/useraction?role=${selectedDeliveryStatus}&page=${currentPage}&items=10`);
+    try {const response = await getRequestHandler(`https://marpapi.techanalyticaltd.com/admin/useraction?role=user`);
+    // try {
+    //   const response = await getRequestHandler(`https://marpapi.techanalyticaltd.com/admin/useraction?role=${roleStatus}&page=${currentPage}&items=10`);
       // Handle the response data
-      setOrderList(response.data.allData)
-       console.log("user response", response.data.allData)
+      setUserList(response.data.allData)
+      
       setTotalPages(response.data.totalPages);
+      // setId(response.data.allData.id)
+      console.log("user response", response.data.allData)
       // setCurrentPage(response.data.currentPage);
       setShow(false)
     } catch (error) {
@@ -55,64 +60,72 @@ export default function OrderList() {
   };
 
   const handleCreateUser = () => {
+    // console.log("handleCreateUser -----");
     swal("Coming Soon!!!", "Please Wait!", "success");
   };
 
-// console.log("orderList--------->",orderList, orderList.length);
+
+
+  const isoDate = "2023-05-29T12:22:50.702Z";
+  const d = new Date(isoDate);
+console.log(d.toLocaleDateString('en-GB'));
+  // console.log("orderList--------->",orderList, orderList.length);
 
   return (
     <>
       {show ?
         <>
-    <>
-          <Backdrop
-            sx={{ color: '#808080', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-            // eslint-disable-next-line no-restricted-globals
-            open={open}
-          >
-            <Circles
-              height="80"
-              width="80"
-              color="#c7eed8"
-              ariaLabel="circles-loading"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={show}
-            /> 
-          </Backdrop>
-        </>
+          <>
+            <Backdrop
+              sx={{ color: '#808080', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+              // eslint-disable-next-line no-restricted-globals
+              open={open}
+            >
+              <Circles
+                height="80"
+                width="80"
+                color="#c7eed8"
+                ariaLabel="circles-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={show}
+              />
+            </Backdrop>
+          </>
         </> :
         <>
-      <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={9}>
-        <Box sx={{ display: "flex", mb: "1rem" }}>
-            <Box sx={{ border: "1px solid #F4F6F8", backgroundColor: "#F4F6F8", borderRadius: 1.1, p: ".5rem", m: ".5rem", cursor: "pointer", boxShadow: 3, '&:hover': { boxShadow: 4 } }} onClick={() => { setSelectedDeliveryStatus("user") }}>Filter</Box>
-            <Box sx={{ border: "1px solid #F4F6F8", backgroundColor: "#F4F6F8", borderRadius: 1.1, p: ".5rem", m: ".5rem", cursor: "pointer", boxShadow: 3, '&:hover': { boxShadow: 4 } }} onClick={() => { setSelectedDeliveryStatus("admin") }}>Admin</Box>
-            <Box sx={{ border: "1px solid #F4F6F8", backgroundColor: "#F4F6F8", borderRadius: 1.1, p: ".5rem", m: ".5rem", cursor: "pointer", boxShadow: 3, '&:hover': { boxShadow: 4 } }} onClick={() => { setSelectedDeliveryStatus("user") }}>User</Box>
-            <Box sx={{ border: "1px solid #F4F6F8", backgroundColor: "#F4F6F8", borderRadius: 1.1, p: ".5rem", m: ".5rem", cursor: "pointer", boxShadow: 3, '&:hover': { boxShadow: 4 } }} onClick={() => { setSelectedDeliveryStatus("shipper") }}>Shipper</Box>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={9}>
+                <Box sx={{ display: "flex", mb: "1rem" }}>
+                  <Box sx={{ border: "1px solid #F4F6F8", backgroundColor: "#F4F6F8", borderRadius: 1.1, p: ".5rem", m: ".5rem", cursor: "pointer", boxShadow: 3, '&:hover': { boxShadow: 4 } }} onClick={() => { setRoleStatus("user") }}>Filter</Box>
+                  <Box sx={{ border: "1px solid #F4F6F8", backgroundColor: "#F4F6F8", borderRadius: 1.1, p: ".5rem", m: ".5rem", cursor: "pointer", boxShadow: 3, '&:hover': { boxShadow: 4 } }} onClick={() => { setRoleStatus("admin") }}>Admin</Box>
+                  <Box sx={{ border: "1px solid #F4F6F8", backgroundColor: "#F4F6F8", borderRadius: 1.1, p: ".5rem", m: ".5rem", cursor: "pointer", boxShadow: 3, '&:hover': { boxShadow: 4 } }} onClick={() => { setRoleStatus("user") }}>User</Box>
+                  <Box sx={{ border: "1px solid #F4F6F8", backgroundColor: "#F4F6F8", borderRadius: 1.1, p: ".5rem", m: ".5rem", cursor: "pointer", boxShadow: 3, '&:hover': { boxShadow: 4 } }} onClick={() => { setRoleStatus("shipper") }}>Shipper</Box>
+                </Box>
+              </Grid>
+              <Grid item xs={3}>
+                <Box sx={{ display: "flex", mb: "1rem" }}>
+                  <Link style={{ textDecoration: "none" }} to="/dashboard/add-user">
+                    <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />} sx={{
+                      bgcolor: "#6610F2", color: "white", ":hover": {
+                        bgcolor: '#6EAB49'
+                      }
+                    }}>
+                      Create New User
+                    </Button>
+                  </Link>
+                </Box>
+              </Grid>
+            </Grid>
           </Box>
-        </Grid>
-        <Grid item xs={3}>
-        <Box sx={{ display: "flex", mb: "1rem" }}>
-            <Link style={{ textDecoration: "none" }} to="/dashboard/add-user">
-           <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />} sx={{bgcolor:"#6610F2", color:"white",":hover": {
-                                bgcolor: '#6EAB49'
-            }}}>
-            Create New User
-          </Button>
-          </Link>
-          </Box>
-      </Grid>
-      </Grid>
-    </Box>
-        
+
 
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
               <TableHead>
                 <TableRow>
-                  <TableCell>Date</TableCell>
+                  <TableCell>Created at</TableCell>
                   <TableCell align="left">Name</TableCell>
                   <TableCell align="left">Role</TableCell>
                   {/* <TableCell align="left">Verified</TableCell> */}
@@ -122,43 +135,50 @@ export default function OrderList() {
                 </TableRow>
               </TableHead>
               <TableBody>
-             {!orderList ?
+                {!userList ?
                   <><Typography>Data Not Found</Typography></>
                   :
                   <>
                     {
-                    orderList.filter((row) =>
-                      selectedDeliveryStatus === 'user'? 
-                         true
-                        : row.role === selectedDeliveryStatus)
-                   .map((row) => (
-                      <TableRow
-                        key={row.id}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                      >
-                        <TableCell component="th" scope="row">
-                          {row.createdAt}
-                        </TableCell>
-                        <TableCell align="left">{row.profile?.fullName}</TableCell>
-                        <TableCell align="left">{row.role}</TableCell>
-                        {row.locked === true ?
-                          <TableCell align="left">
-                            <Typography sx={{ display: 'flex', alignItems: "center", border: '1px solid #F4F6F8', borderRadius: 1, inlineSize: 'fit-content', p: ".2rem", bgcolor: "#F4F6F8", }}>
-                              <span style={{ color: '#6EAB49', fontSize: "2rem", lineHeight: '0.35' }}>•</span>Active
-                              {row.locked}
-                            </Typography>
-                          </TableCell>
-                          :
-                          <TableCell align="left">
-                          <Typography sx={{ display: 'flex', alignItems: "center", border: '1px solid #F4F6F8', borderRadius: 1, inlineSize: 'fit-content', p: ".2rem", bgcolor: "#F4F6F8", }}>
-                            <span style={{ color: 'red', fontSize: "2rem", lineHeight: '0.35' }}>•</span>Locked
-                            {row.locked}
-                          </Typography>
-                        </TableCell>
-                        }
-                        <TableCell align="left"><EditIcon sx={{color:"#6EAB49"}}/></TableCell>
-                      </TableRow>
-                    ))}
+                      userList.filter((row) =>
+                      roleStatus === 'user' ?
+                          true
+                          : row.role === roleStatus)
+                        .map((row) => (
+                          <TableRow
+                            key={row.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                          >
+                            <TableCell component="th" scope="row">
+                              {
+                              new Date(row.createdAt).toLocaleDateString('en-GB')
+                              }
+                            </TableCell>
+                            <TableCell align="left">{row.profile?.fullName}</TableCell>
+                            <TableCell align="left">{row.role}</TableCell>
+                            {row.locked === false ?
+                              <TableCell align="left">
+                                <Typography sx={{ display: 'flex', alignItems: "center", border: '1px solid #F4F6F8', borderRadius: 1, inlineSize: 'fit-content', p: ".2rem", bgcolor: "#F4F6F8", }}>
+                                  <span style={{ color: '#6EAB49', fontSize: "2rem", lineHeight: '0.35' }}>•</span>Active
+                                  {row.locked}
+                                </Typography>
+                              </TableCell>
+                              :
+                              <TableCell align="left">
+                                <Typography sx={{ display: 'flex', alignItems: "center", border: '1px solid #F4F6F8', borderRadius: 1, inlineSize: 'fit-content', p: ".2rem", bgcolor: "#F4F6F8", }}>
+                                  <span style={{ color: 'red', fontSize: "2rem", lineHeight: '0.35' }}>•</span>Locked
+                                  {row.locked}
+                                </Typography>
+                              </TableCell>
+                            }
+                            <TableCell align="left">
+
+                              <Link to={`/dashboard/add-user/${row.id}`}>
+                                <EditIcon sx={{ color: "#6EAB49" }} />
+                              </Link>
+                            </TableCell>
+                          </TableRow>
+                        ))}
                   </>}
               </TableBody>
             </Table>
